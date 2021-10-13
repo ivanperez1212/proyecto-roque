2b52fc797a22a71e3e4c99bc27beb71808662a41
ivan <ivanperezjimenez1l40@outlook.es>
2021-10-13 11:05:56 -0500

    prueba

---
+++ frontend-store-online/src/components/Articulo.js
@@ -0,0 +1,13 @@
+import React from 'react';
+class Articulo extends React.Component{
+   
+    render() { 
+        return ( 
+            <div>
+<h1> Articulos</h1>
+            </div>
+         );
+    }
+}
+ 
+export default Articulo;
